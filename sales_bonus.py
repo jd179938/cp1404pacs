e9804f369d6fbd5f58f76ad696b046c038f21d88
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-05-31 09:42:29 +1000

    Loop to calculates multiple sales bonus

--- sales_bonus.py
+++ sales_bonus.py
@@ -3,12 +3,22 @@ If sales are under $1,000, the user gets a %10 bonus.
 If sales are $1,000 or over, the bonus is %15.
 """
 
+# sales = float(input("Enter sales: $"))
+# while sales < 0:
+#     print("Invalid input")
+#     sales = float(input("Enter sales: $"))
+# if sales < 1000:
+#     bonus = sales * 0.1
+# else:
+#     bonus = sales * 0.15
+# print(f"Total bonus is ${bonus:0.2f}")
+
+# version with loop
 sales = float(input("Enter sales: $"))
-while sales < 0:
-    print("Invalid input")
+while sales >= 0:
+    if sales < 1000:
+        bonus = sales * 0.1
+    else:
+        bonus = sales * 0.15
+    print(f"Total bonus is {bonus:0.2f}")
     sales = float(input("Enter sales: $"))
-if sales < 1000:
-    bonus = sales * 0.1
-else:
-    bonus = sales * 0.15
-print(f"Total bonus is ${bonus:0.2f}")
