f322d300bc8231d649d531a81ec5dd8b8ba603fa
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-05-31 16:09:44 +1000

    Generate extra random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+from random import randint
 
 def main():
     """ Get score and display result."""
@@ -9,7 +10,9 @@ def main():
     while score < 0 or score > 100:
         print("Invalid score!")
         score = float(input("Enter score: "))
-    print(determine_result(score))
+    print(f"User score {score} is {determine_result(score)}")
+    random_score = randint(1,100)
+    print(f"Random: {random_score} = {determine_result(random_score)}")
 
 def determine_result(score):
     """Determine result based on score."""
