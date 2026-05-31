45b6c79108f7d47416ee2433e528ebc0f2c41f94
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-05-31 14:25:45 +1000

    Part a,b,c,d working loops

---
+++ prac_01/loops.py
@@ -0,0 +1,23 @@
+for i in range(1, 21, 2):
+    print(i, end=" ")
+print()
+
+# a
+for i in range(0, 101, 10):
+    print(i, end=" ")
+print()
+
+# b
+for i in range(20, 0, -1):
+    print(i, end=" ")
+print()
+
+# c
+number_of_stars = int(input("Number of stars: "))
+while number_of_stars < 0:
+    print("Invalid number of stars")
+    number_of_stars = int(input("Number of stars: "))
+print("*" * number_of_stars)
+#d
+for i in range(1, number_of_stars + 1):
+    print("*" * i)
