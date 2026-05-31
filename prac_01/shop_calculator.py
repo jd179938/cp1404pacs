95818302aa63444a64a88a615623963cd8e3fa92
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-05-31 14:31:14 +1000

    Discount added if total cost is large

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,5 +1,5 @@
 """
-Shop calculator to determain total price
+Shop calculator to determine total price
 """
 total_cost = 0
 number_of_items = int(input("Number of items: "))
@@ -9,4 +9,7 @@ while number_of_items < 0:
 for i in range(number_of_items):
     price_of_item = float(input("Price of item: $"))
     total_cost = total_cost + price_of_item
+if total_cost > 100:
+    discount = total_cost * 0.1
+    total_cost = total_cost - discount
 print(f"Total price for {number_of_items} items is {total_cost:0.2f}")
