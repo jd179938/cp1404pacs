ca7526e0ffd07cf95081ed6ed637d79dfd47189e
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-05-31 15:41:15 +1000

    Refactored conversion equations into seperate functions

--- prac_02/conver_temps.py
+++ prac_02/conver_temps.py
@@ -1,3 +1,34 @@
 """
-Convert celsius to fahrenheit
+CP1404/CP5632 - Practical
+Program for temperature conversion
 """
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = fahrenheight_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def celsius_to_fahrenheit(temperture_celsius):
+    return temperture_celsius * 9.0 / 5 + 32
+
+def fahrenheight_to_celsius(temperature_fahrehit):
+    return 5 / 9 * (temperature_fahrehit - 32)
+
+main()
