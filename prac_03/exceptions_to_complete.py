59f4a5e7d50c9fd3e7a05ef75166778d94f081a3
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-06-06 10:23:15 +1000

    Catch Value Errors from input

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True
+    except ValueError as error:
+        print(f"Please enter a valid integer. {error}")
+print("Valid result is:", result)
